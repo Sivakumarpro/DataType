56305153083e423617004dfba75fe67f2b6f6dd7
Sivakumarpro <sivaexcel.10@gmail.com>
2022-08-29 10:01:59 +0530

    task

---
+++ src/org/java/EmployeeDetails.java
@@ -0,0 +1,32 @@
+package org.java;
+
+public class EmployeeDetails {
+	public static void main(String[] args) {
+		//datatype variablename=value;
+		byte age=20;
+		System.out.println("Employee age is "+age);
+		
+		short year=1234;
+		System.out.println("Employee date of birth is "+year);
+	
+		int id=12345;
+		System.out.println("Employee id card number is "+id);
+	
+		long accNo=12345678956l;
+		System.out.println("Employee acconut number is "+accNo);
+	
+		float marks=80.21f;
+		System.out.println("Employee mathethamatics mark is "+marks);
+		
+		double salary=12312.12354;
+		System.out.println("Employee monthly salary is "+salary);
+	
+		char gender='M';
+		System.out.println("Employee gender "+gender);
+		
+		String name="Siva";
+		System.out.println("Employee name is "+name);
+		
+		boolean name1=false;
+		System.out.println("First name siva is true "+name1);
+	}}
